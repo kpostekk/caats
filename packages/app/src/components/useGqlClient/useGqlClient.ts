9a7fadd70296d02e39462398e8f0b782e872ebc0
Krystian Postek <krystian@postek.eu>
2022-12-31 02:16:08 +0100

    [app] fix: logout if server responds with unathorized response

--- packages/app/src/components/useGqlClient/useGqlClient.ts
+++ packages/app/src/components/useGqlClient/useGqlClient.ts
@@ -1,7 +1,14 @@
 import { GraphQLClient } from 'graphql-request'
 import { useAuthStore } from '../../states/auth'
 
-export const gqlClient = new GraphQLClient('/graphql')
+export const gqlClient = new GraphQLClient('/graphql', {
+  responseMiddleware: (res) => {
+    if (!(res instanceof Error)) return
+    if (res.message.startsWith('Unauthorized')) {
+      useAuthStore.getState().logout()
+    }
+  },
+})
 
 export function useGqlClient() {
   const token = useAuthStore(({ auth }) => auth?.accessToken)
