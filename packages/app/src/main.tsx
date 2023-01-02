d80a691c47d203f5105151f04922dbe2e45b77c3
Krystian Postek <krystian@postek.eu>
2023-01-02 23:52:45 +0100

    [app] fix: fix router typo

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './rourter'
+import { router } from './router'
 import { Settings } from 'luxon'
 import './index.css'
 
