a032ea8ec37e45621497d940ef530ba4462399bb
Krystian Postek <krystian@postek.eu>
2023-09-17 13:23:24 +0200

    fix: am i a joke ?

--- packages/scrapy/src/gql/sdk.ts
+++ packages/scrapy/src/gql/sdk.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request';
-import * as Dom from 'graphql-request/dist/types.dom';
+// import * as Dom from 'graphql-request/dist/types.dom';
 import gql from 'graphql-tag';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
